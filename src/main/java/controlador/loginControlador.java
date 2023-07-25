4e5d6ef7b409d9649244af02f4d80c452fc935f6
mnbenavides <mnbenavides1@gmail.com>
2023-07-25 13:13:48 -0500

    Descripcion: Version inicial del rpoyecto maven agregando como dependencia JSF  y se crea el login del aplicativo y se redirecciona a la pantalla principal

---
+++ src/main/java/controlador/loginControlador.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package controlador;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+/**
+ * @author NATALIA
+ * 
+ */
+@ManagedBean(name = "login")
+public class loginControlador {
+	/**
+	 * Usuario ingresado en el login
+	 */
+	private String usuario;
+	/**
+	 * Contraseña ingresada en el login
+	 */
+	private String contrasenia;
+
+	/**
+	 * @return the usuario
+	 */
+	public String getUsuario() {
+		return usuario;
+	}
+
+	/**
+	 * @param usuario the usuario to set
+	 */
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	/**
+	 * @return the contrasenia
+	 */
+	public String getContrasenia() {
+		return contrasenia;
+	}
+
+	/**
+	 * @param contrasenia the contrasenia to set
+	 */
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+
+	/**
+	 * Metodo que permite ingresar a la pantalla principal del proyecto
+	 */
+	public void ingresar() {
+		if (usuario.equals("desarrollador") && contrasenia.equals("123456789")) {
+			this.redireccionar("principal.xhtml");
+		} else {
+			String stringMessage = "Security has been incorrectly";
+			FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, stringMessage, stringMessage);
+			FacesContext.getCurrentInstance().addMessage("formLogin.txtUsuario", message);
+		}
+	}
+
+	public void redireccionar(String pagina) {
+		ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext(); // Permite redireccionar a otra
+																						// pagina
+		try {
+			ec.redirect(pagina);
+		} catch (Exception e) {
+			String stringMessage = "Ocurrio un error al redireccionar la pagina";
+			FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_FATAL, stringMessage, stringMessage);
+			FacesContext.getCurrentInstance().addMessage("formLogin.txtUsuario", message);
+		}
+	}
+
+}
